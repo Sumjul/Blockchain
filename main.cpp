d63352e3da4f7c117795395eb7c0584bb26f1ab0
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-10-20 21:42:28 +0300

    Realizuota transakcijų generavimo funkcija

--- main.cpp
+++ main.cpp
@@ -1,42 +1,69 @@
 #include "main.h"
 
-void GenerateUsers () {
+// Randomizers
+mt19937 rndDev(random_device{}());
+uniform_int_distribution<uint64_t> rndBalance(100, 1000000);
+uniform_int_distribution<uint64_t> rndAmount(0, 100000);
+uniform_int_distribution<int> rndUser(0, 999);
+uniform_int_distribution<int> rndLetter('A', 'Z');
+
+// Functions
+vector<User> GenerateUsers() {
     vector<User> users;
     users.reserve(1000);
-
-    mt19937 rndDev(random_device{}());
-    uniform_int_distribution<uint64_t> rndBalance(100, 1000000);
-    uniform_int_distribution<int> rndLetters('A', 'Z');
 
     ofstream outFile("data/users.txt");
     if (!outFile.is_open()) {
         cerr << "Nera failo duomenu irasymui!" << endl;
-        return;
+        return vector<User>();
     }
 
     for (int i = 0; i < 1000; ++i)
     {
         string name = "User_" + to_string(i + 1);
         string key = "PUBKEY_";
-        for (int j = 0; j < 16; ++j) key += static_cast<char>(rndLetters(rndDev));
+        for (int j = 0; j < 16; ++j) key += static_cast<char>(rndLetter(rndDev));
         uint64_t  balance = rndBalance(rndDev);
 
-        users.emplace_back(name, key, balance);
-        outFile << users[i].getName() << "," << users[i].getKey() << "," << users[i].getBalance() << endl;
-        cout << "Vardas: " << users[i].getName() << " | Raktas: " << users[i].getKey() << " | Balansas: " << users[i].getBalance() << endl;
+        User u(name, key, balance);
+        users.emplace_back(u);
+        outFile << u.getName() << "," << u.getKey() << "," << u.getBalance() << endl;
+        cout << "Vardas: " << u.getName() << " | Raktas: " << u.getKey() << " | Balansas: " << u.getBalance() << endl;
     }
     cout << "Is viso vartotoju sugeneruota: " << users.size() << endl;
+    outFile.close();
+    return users;
+}
+
+void GenerateTransaction(const vector<User>& users) {
+    vector<Transaction> transactions;
+    transactions.reserve(10000);
+
+    ofstream outFile("data/transactions.txt");
+    if (!outFile.is_open()) {
+        cerr << "Nera failo duomenu irasymui!" << endl;
+        return;
+    }
+
+    for (int i = 0; i < 10000; ++i)
+    {
+        int senderInd = rndUser(rndDev);
+        int receiverInd = rndUser(rndDev);
+        while (receiverInd == senderInd) receiverInd = rndUser(rndDev);
+        uint64_t amount = rndAmount(rndDev);
+
+        Transaction t(users[senderInd].getKey(), users[receiverInd].getKey(), amount);
+        transactions.emplace_back(t);
+        outFile << t.getID() << "," << t.getSender() << "," << t.getReceiver() << "," << t.getAmount() << endl;
+        cout << t.getID() << " | " << t.getSender() << " -> " << t.getReceiver() << " : " << t.getAmount() << endl;
+    }
+    cout << "Is viso transakciju sugeneruota: " << transactions.size() << endl;
     outFile.close();
 }
 
 int main()
 {
-    //GenerateUsers();
-
-    //1 testas
-    Transaction t("PUBKEY_ABC", "PUBKEY_XYZ", 500);
-    cout << t.getID() << " | " << t.getSender() << " -> " << t.getReceiver() << " : " << t.getAmount() << endl;
-
-
+    vector<User> users = GenerateUsers();
+    GenerateTransaction(users);
     return 0;
 }
